2b553c35243be6c4cd4676b1d3754f2b4b991d36
Hu Chen <huch0003@gmail.com>
2013-08-09 12:15:28 +0800

    new post client side validation

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
-
+  $("#new_post_field").on("keyup", function(e) {
+    var max = parseInt($(this).attr("maxlength"));
+    if ($(this).val().length > max) {
+      $(this).val($(this).val().substr(0, $(this).attr("maxlength")));
+    }
+    $(".charsRemaining").html("" + (max - $(this).val().length));
+    if ($(this).val().length > 0) {
+      $('#submit').toggleClass('disabled', false);
+    } else {
+      $('#submit').toggleClass('disabled', true);
+    }
+  });
 });
